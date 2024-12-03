4cf4271873f3dfae4a4068a1521c90a6e58e4d4e
falibabaei <f.alibabaee@gmail.com>
2024-12-03 11:46:51 +0100

    update git action

--- ai4life/filter_v0_5_models.py
+++ ai4life/filter_v0_5_models.py
@@ -1,23 +1,30 @@
+import os
 
 import json
 from bioimageio.core import load_description
 from bioimageio.spec.model import v0_5
-
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
-        return str(obj)
+        # Handle the case if the object type is unknown or non-serializable
+        return str(obj) 
 
-def filter_and_load_models(input_json, output_json, perform_io_checks=False):
+def filter_and_load_models(input_json='collection.json', 
+                           output_json='filtered_models.json',
+                           perform_io_checks= False):
+    # Load the JSON file
     with open(input_json, 'r') as file:
         data = json.load(file)
-
+        
+    # Filter entries where "type" is "model"
+  
     models = [entry for entry in data['collection'] if entry['type'] == 'model']
+  
     models_v0_5 = {}
 
     for model_entry in models:
         model_id = None
         model = None
-
+        
         if model_entry.get('concept'):
             model_id = model_entry['concept']
         elif model_entry.get('concept_doi'):
@@ -26,16 +33,31 @@ def filter_and_load_models(input_json, output_json, perform_io_checks=False):
             model_id = model_entry['source']
 
         if model_id:
-            model = load_description(model_id, perform_io_checks=perform_io_checks)
+             
+            model = load_description(model_id, 
+                                     perform_io_checks=perform_io_checks)
 
             if isinstance(model, v0_5.ModelDescr):
-                print(f"The model '{model.name}' with ID '{model_id}' has been loaded.")
+                print(f"\nThe model '{model.name}' with ID '{model_id}' has been correctly loaded.")
+                # Store model information in a dictionary
+                #model_io_info  = get_model_io_info(model)
                 combined_entry = {**model_entry}
+            
+                # Store the combined entry in the models_v0_5 dictionary
                 models_v0_5[model_id] = combined_entry
 
-    with open(output_json, 'w') as names_file:
-        json.dump(models_v0_5, names_file, indent=4, cls=CustomEncoder)
+                for weight in model.weights:
+                    weight_format, weight_info = weight
+                    #We support pytorch weights
+                    #for now 
+                   # if (weight_format == 'torchscript' or weight_format == 'pytorch_state_dict') and weight_info is not None:
 
+                             # Define output path
+                    #names_output_json = os.path.join(config.MODELS_PATH, 'models_v0_5.json')        
+    
+    # Write all model info to a JSON file
+    with open(output_json, 'w') as names_file:
+        json.dump(models_v0_5, names_file, indent=4, cls=CustomEncoder)        
     return models_v0_5
 
 if __name__=='__main__':
