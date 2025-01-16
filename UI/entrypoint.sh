ded7f50535e844449cc042affc9ef4184ef3bd7c
Khadijeh Alibabaei <khadijeh.alibabaei@kit.edu>
2025-01-16 12:41:20 +0100

    Update entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -1,9 +1,11 @@
-for i in {1..5}; do
-    (
-        echo "Iteration $i:"
-        deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
-        sleep 10
-        python3 ./UI/launch.py --api_url http://0.0.0.0:5000/ --ui_port 8000
-    ) &
-done
-wait
+#!/bin/bash
+set -e
+
+# Start DeepAAS
+deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
+
+# Wait for DeepAAS to start
+sleep 40
+
+# Launch Gradio UI
+python3 ./UI/launch.py --api_url http://0.0.0.0:5000/ --ui_port 8000
