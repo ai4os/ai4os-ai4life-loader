c0fb1d9b5918929751ff29e49ff0e00a2275e475
Khadijeh Alibabaei <khadijeh.alibabaei@kit.edu>
2025-01-16 12:31:46 +0100

    Update entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -1,9 +1,9 @@
 for i in {1..5}; do
     (
         echo "Iteration $i:"
-        deepaas-run --listen-ip 0.0.0.0 --listen-port $((5000 + i)) &
+        deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
         sleep 10
-        python3 ./UI/launch.py --api_url http://0.0.0.0:$((5000 + i))/ --ui_port $((8000 + i))
+        python3 ./UI/launch.py --api_url http://0.0.0.0:5000/ --ui_port 8000
     ) &
 done
 wait
