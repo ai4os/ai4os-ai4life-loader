9bfaf6fb29404a544d1f2f122956790fe802c1ad
Khadijeh Alibabaei <khadijeh.alibabaei@kit.edu>
2025-01-16 12:18:39 +0100

    Update entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -3,7 +3,6 @@
 deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
 
 # Wait briefly to ensure DeepAAS is running
-sleep 5
-
+sleep 60
 # Start Gradio UI, pointing to DeepAAS endpoint
 python3 ai4-ai4life//UI/launch.py --api_url http://0.0.0.0:5000/ --ui_port 80
