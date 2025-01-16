70c08bb5d33627f50afdf2d40329d0305bccf28d
Khadijeh Alibabaei <khadijeh.alibabaei@kit.edu>
2025-01-16 12:52:30 +0100

    Update entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -8,4 +8,4 @@ deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
 sleep 40
 
 # Launch Gradio UI
-python3 ./UI/launch.py --api_url http://0.0.0.0:5000/ --ui_port 8000
+python3 ai4-ai4life/UI/launch.py --api_url http://0.0.0.0:5000/ --ui_port 8000
