947580e140b3c6d2e5b836ae89a0a7a19d0ce21e
Khadijeh Alibabaei <khadijeh.alibabaei@kit.edu>
2025-02-06 12:00:30 +0100

    Edit entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -4,6 +4,9 @@ deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
 
 # Wait briefly to ensure DeepAAS is running
 sleep 5
-
+SCRIPT_DIR=$(dirname "$0")
+echo "Script directory is: $SCRIPT_DIR"
 # Start Gradio UI, pointing to DeepAAS endpoint
-python3 ./UI/launch.py --api_url http://0.0.0.0:5000/ --ui_port 80
+python3 "${SCRIPT_DIR}/UI/launch.py" \
+    --api_url "http://0.0.0.0:5000/" \
+    --ui_port 8080
