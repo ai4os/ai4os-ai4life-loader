e32f90e35accf3ebe43a7ee0d5692990aaf23cdd
falibabaei <66964597+falibabaei@users.noreply.github.com>
2025-02-06 17:06:07 +0100

    Update entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -15,4 +15,4 @@ echo "Script directory is: $SCRIPT_DIR"
 # Start Gradio UI, pointing to DeepAAS endpoint
 python3 "${SCRIPT_DIR}/UI/launch.py" \
     --api_url "http://0.0.0.0:5000/" \
-    --ui_port 8080
+    --ui_port 80
