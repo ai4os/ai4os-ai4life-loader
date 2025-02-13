c7bd31e6ec55ddc15c85efdbbf3b16c10c9059cd
falibabaei <66964597+falibabaei@users.noreply.github.com>
2025-02-13 18:06:58 +0100

    Update entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -3,11 +3,8 @@
 deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
 
  
-# Wait for DeepAAS to start
-until curl --output /dev/null --silent --head --fail http://0.0.0.0:5000; do
-    echo "Waiting for DeepAAS to start..."
-    sleep 2
-done
+ 
+sleep 40
 # Get the absolute script directory
 SCRIPT_DIR=$(dirname "$0")
 echo "Script directory is: $SCRIPT_DIR"
