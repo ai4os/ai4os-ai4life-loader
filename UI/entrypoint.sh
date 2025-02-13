6a8c0bf933c0a73dce23a74d59b381c1758b4da4
falibabaei <66964597+falibabaei@users.noreply.github.com>
2025-02-13 08:43:50 +0100

    Update entrypoint.sh

--- UI/entrypoint.sh
+++ UI/entrypoint.sh
@@ -4,16 +4,16 @@ deepaas-run --listen-ip 0.0.0.0 --listen-port 5000 &
 
  
 # Wait for DeepAAS to start
-sleep 40
-# Launch Gradio UI
- 
+until curl --output /dev/null --silent --head --fail http://0.0.0.0:5000; do
+    echo "Waiting for DeepAAS to start..."
+    sleep 2
+done
 # Get the absolute script directory
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 echo "Script directory is: $SCRIPT_DIR"
 
 
-
 # Start Gradio UI, pointing to DeepAAS endpoint
-python3 "${SCRIPT_DIR}/UI/launch.py" \
+python3 "${SCRIPT_DIR}/launch.py" \
     --api_url "http://0.0.0.0:5000/" \
     --ui_port 80
